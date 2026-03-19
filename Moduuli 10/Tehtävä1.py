019dd7a5fc8a39450f4319539d3ca88d6fe2f986
Valtteri Piironen <valtteri.piironen98@gmail.com>
2026-03-19 19:38:23 +0200

    muutoksia Ohjelmisto2.iml

---
+++ Moduuli 10/Tehtävä1.py
@@ -0,0 +1,7 @@
+class Elevator:
+    def __init__(self, go_to_floor, floor_up, floor_down):
+        self.go_to_floor = go_to_floor
+        self.floor_up = floor_up
+        self.floor_down = floor_down
+
+
