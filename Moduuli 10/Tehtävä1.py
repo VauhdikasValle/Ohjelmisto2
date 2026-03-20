d81c99870c268dc9c35cd47b82d1c5596e9a2e5f
Valtteri <valtteri.piironen98@gmail.com>
2026-03-20 11:26:59 +0200

    muutoksia ohjelmisto 2

--- Moduuli 10/Tehtävä1.py
+++ Moduuli 10/Tehtävä1.py
@@ -1,7 +1,15 @@
 class Elevator:
-    def __init__(self, go_to_floor, floor_up, floor_down):
-        self.go_to_floor = go_to_floor
-        self.floor_up = floor_up
-        self.floor_down = floor_down
+    def __init__(self, bottom_floor, top_floor):
+        self.current_floor = bottom_floor
+        self.top_floor = top_floor
+        self.bottom_floor = bottom_floor
+
+    def go_to_floor(self, floor):
+        new_floor = self.current_floor + floor
+        if new_floor
+
+
+
+
 
 
