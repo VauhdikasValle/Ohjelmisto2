54297a299d922999dbf177387b192ced1595b666
Valtteri Piironen <valtteri.piironen98@gmail.com>
2026-03-31 14:16:44 +0300

    muutoksia Ohjelmisto2.iml

--- Moduuli 10/Tehtävä1.py
+++ Moduuli 10/Tehtävä1.py
@@ -1,21 +1,30 @@
 class Elevator:
     def __init__(self, bottom_floor, top_floor):
         self.current_floor = bottom_floor
-        self.top_floor = top_floor
         self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+
+    def floor_up(self):
+        self.current_floor += 1
 
     def go_to_floor(self, floor):
-        self.current_floor = self.current_floor + floor
-        if new_floor
+        if floor < self.bottom_floor or floor > self.top_floor:
+            return
 
-    def floor_up
-        self.current_floor = self.current_floor + 1
+        if self.current_floor < floor:
+            for i in range(floor - self.current_floor):
+                self.floor_up()
+                print(f"Current floor: {self.current_floor}")
+        elif self.current_floor > floor:
+            for i in range(self.current_floor - floor):
+                self.floor_down()
+                print(f"Current floor: {self.current_floor}")
+        else:
+            print(f"Current floor: {self.current_floor}")
+            return
 
-    def floor_down
-        self.current_floor = self.current_floor + 1
-
-
-        repeat tää kunnes saavutan floor x
+    def floor_down(self):
+        self.current_floor -= 1
 
 
 
