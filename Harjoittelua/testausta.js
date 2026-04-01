e6c8b3458a9ace548207ef8ee4e1965b86fae735
Valtteri <valtteri.piironen98@gmail.com>
2026-04-01 14:14:40 +0300

    muutoksia ohjelmisto 2

---
+++ Harjoittelua/testausta.js
@@ -0,0 +1,28 @@
+ // const names = ['Frank', 'Scott', 'Jasmine', 'Don'];
+ //
+ //    for (let i = 0; i < names.length; i++) {
+ //        console.log(`Name: ${names[i]}`);
+ //    }
+ //
+
+
+
+   const n1 = 5; // global variable
+
+        function hello() {
+            var n2 = 5; // an internal variable of a function
+
+            if (n2>0) {
+                const n3 = 5; // an internal variable of a block
+                var n4 = 5; // an internal variable of a function
+            }
+            console.log(n1); // global variable is visible everywhere
+            console.log(n2); // the internal variable is aviailable inside the function
+            //console.log(n3); -- an internal variable of a block is not available outside the function
+            console.log(n4); // the internal variable of the function is available inside the function
+
+        }
+
+        hello();
+
+        console.log(n1);
