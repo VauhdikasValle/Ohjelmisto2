e3c82c699e9a077207c35777619a42a1c4b4f394
Valtteri <valtteri.piironen98@gmail.com>
2026-04-01 14:39:04 +0300

    muutoksia ohjelmisto 2

--- Harjoittelua/testausta.js
+++ Harjoittelua/testausta.js
@@ -7,22 +7,23 @@
 
 
 
-   const n1 = 5; // global variable
+   // const n1 = 5; // global variable
+ //    //
+ //    //      function hello() {
+ //    //          var n2 = 5; // an internal variable of a function
+ //    //
+ //    //          if (n2>0) {
+ //    //              const n3 = 5; // an internal variable of a block
+ //    //              var n4 = 5; // an internal variable of a function
+ //    //          }
+ //    //          console.log(n1); // global variable is visible everywhere
+ //    //          console.log(n2); // the internal variable is aviailable inside the function
+ //    //          //console.log(n3); -- an internal variable of a block is not available outside the function
+ //    //          console.log(n4); // the internal variable of the function is available inside the function
+ //    //
+ //    //      }
+ //    //
+ //    //      hello();
+ //    //
+ //    //      console.log(n1);
 
-        function hello() {
-            var n2 = 5; // an internal variable of a function
-
-            if (n2>0) {
-                const n3 = 5; // an internal variable of a block
-                var n4 = 5; // an internal variable of a function
-            }
-            console.log(n1); // global variable is visible everywhere
-            console.log(n2); // the internal variable is aviailable inside the function
-            //console.log(n3); -- an internal variable of a block is not available outside the function
-            console.log(n4); // the internal variable of the function is available inside the function
-
-        }
-
-        hello();
-
-        console.log(n1);
