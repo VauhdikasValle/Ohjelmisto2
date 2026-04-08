40d77f5c2623efdc52c5e7468ee1cd78893cafe5
Valtteri Piironen <valtteri.piironen98@gmail.com>
2026-04-08 18:32:33 +0300

    muutoksia Ohjelmisto2.iml

--- Harjoittelua/testausta.js
+++ Harjoittelua/testausta.js
@@ -1,29 +1,7 @@
- // const names = ['Frank', 'Scott', 'Jasmine', 'Don'];
- //
- //    for (let i = 0; i < names.length; i++) {
- //        console.log(`Name: ${names[i]}`);
- //    }
- //
+function happyBirthday(username, age){
+    console.log("HBD!");
+    console.log(`happy HBD ${username}`);
+    console.log(`you are ${age} years old`);
 
 
-
-   // const n1 = 5; // global variable
- //    //
- //    //      function hello() {
- //    //          var n2 = 5; // an internal variable of a function
- //    //
- //    //          if (n2>0) {
- //    //              const n3 = 5; // an internal variable of a block
- //    //              var n4 = 5; // an internal variable of a function
- //    //          }
- //    //          console.log(n1); // global variable is visible everywhere
- //    //          console.log(n2); // the internal variable is aviailable inside the function
- //    //          //console.log(n3); -- an internal variable of a block is not available outside the function
- //    //          console.log(n4); // the internal variable of the function is available inside the function
- //    //
- //    //      }
- //    //
- //    //      hello();
- //    //
- //    //      console.log(n1);
-
+happyBirthday("Muru", 30)
