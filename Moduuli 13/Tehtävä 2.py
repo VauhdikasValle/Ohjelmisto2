1b409d714cfa90d95da9b92183fc37142aadebfc
Valtteri Piironen <valtteri.piironen98@gmail.com>
2026-04-14 20:42:27 +0300

    muutoksia Ohjelmisto2.iml

--- Moduuli 13/Tehtävä 2.py
+++ Moduuli 13/Tehtävä 2.py
@@ -1,7 +1,29 @@
+from flask import Flask
+app = Flask(__name__)
+import mysql.connector
+
+connection = mysql.connector.connect(
+            host='127.0.0.1',
+            database='flight_game',
+            user='Valtteri',
+            password='Pallero1234'
+        )
+
 @app.route('/kenttä/<icao>')
 def hae_kentta(icao):
-    # 1. Tee SQL-haku tietokantaan
-    # 2. Tallenna tulokset muuttujiin
-    # 3. Palauta JSON:
-    # return {"ICAO": icao, "Name": nimi, "Municipality": kaupunki}
-    pass
+    cursor = connection.cursor()
+    sql = "SELECT name, municipality FROM airport WHERE ident = %s"
+    cursor.execute(sql, (icao,))
+    tulos = cursor.fetchone()
+
+    if not tulos:
+        return {"Error": f"Lentokenttää koodilla {icao} ei löytynyt."}
+
+
+    return {
+        "ICAO": icao,
+        "Name": tulos[0],
+        "Municipality": tulos[1]
+    }
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
