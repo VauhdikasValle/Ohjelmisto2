58742cdeeec81ac78604c2f46bac329bc6d678e5
Valtteri <valtteri.piironen98@gmail.com>
2026-04-15 11:51:47 +0300

    muutoksia ohjelmisto 2

--- Harjoittelua/harjoittelu.py
+++ Harjoittelua/harjoittelu.py
@@ -1,8 +1,17 @@
-class Nainen:
+# class Nainen:
+#     pass
+#
+# nainen = Nainen()
+# nainen.nimi = "Linnea"
+# nainen.syntymävuosi = 1994
+#
+# print (f"{nainen.nimi} on syntynyt vuonna {nainen.syntymävuosi}." )
+
+
+class Opiskelija:
     pass
+opiskelija = Opiskelija()
+opiskelija.nimi = Juhis
+opiskelija.opintopisteet = 100
 
-nainen = Nainen()
-nainen.nimi = "Linnea"
-nainen.syntymävuosi = 1994
-
-print (f"{nainen.nimi} on syntynyt vuonna {nainen.syntymävuosi}." )
+print(f"")
